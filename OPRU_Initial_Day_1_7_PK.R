ba3061ca5a54d35f62189946273e10f850fe0b24
PVDodeja <prernadodeja@gmail.com>
2023-10-05 15:24:08 -0400

    Refine

--- OPRU_Initial_Day_1_7_PK.R
+++ OPRU_Initial_Day_1_7_PK.R
@@ -1,4 +1,6 @@
-rm(list = ls(all.names= TRUE)) #
+#-----17-OHPC Data Exploration----------#
+
+rm(list = ls(all.names= TRUE)) #clear environment
 
 ## Load libraries used in this script
 library(ggplot2) # For plotting
@@ -13,20 +15,36 @@ getwd()
 # --------------
 raw<- read.csv("OPRU_PK_Day1_7_Raw.csv")
 
-
 #filter the data
 raw_no_missing<- filter(raw, DV!=".") #remove the DVs that have missing value
 
-# Create Individual plots
-plot_IND <- 
-  ggplot(data=raw_no_missing, aes(x = TIME, y = DV)) +
-  theme_cowplot()+
-  geom_point(data = raw_no_missing, aes(x = TIME, y=DV))+
-  #geom_line(data = raw_no_missing, aes(x = TIME, y=DV))+
-  facet_wrap(~ID)+    #separating the plots by subject
-  ylab("17-OHPC Concentration (ng/mL)")+
-  xlab("Time in days")+
-  ggtitle("Data Exploration", subtitle="OPRU study")
-plot_IND
+#DV is considered a string, so change to numeric--- ELSE ERRORS
+raw_no_missing$DV= as.numeric(raw_no_missing$DV)
 
+#Data Exploration By ID-Linear Scale
+plot_IND <- 
+  ggplot()+
+  geom_point(data=raw_no_missing, aes(x = TIME, y=DV))+
+  geom_line(data = raw_no_missing, aes(x = TIME, y=DV))+
+  facet_wrap(~ID)+    #separating the plots by subject+
+  scale_y_continuous(limits=c(0,60))+
+  ylab("17-OHPC Concentration (ng/mL)")+
+  xlab("Time (days)")+
+  theme_cowplot()+
+  ggtitle("Data Exploration", subtitle="Linear Scale")
+plot_IND
 #ID 57 and 52 look best
+
+#Data Exploration By ID-Log Scale
+plot_IND_log <- 
+  ggplot()+
+  geom_point(data=raw_no_missing, aes(x = TIME, y=DV))+
+  geom_line(data = raw_no_missing, aes(x = TIME, y=DV))+
+  facet_wrap(~ID)+    #separating the plots by subject
+  scale_y_log10()+
+  scale_x_continuous(limits=c(0,8))+
+  ylab("17-OHPC Concentration (ng/mL)")+
+  xlab("Time (days)")+
+  theme_cowplot()+
+  ggtitle("Data Exploration", subtitle="Log10 Scale")
+plot_IND_log
